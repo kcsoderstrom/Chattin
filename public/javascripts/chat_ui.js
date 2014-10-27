0e12e2d82fd62c342f32aabec3974d99ddbfb99d
App Academy Student <student@appacademy.io>
2014-10-27 18:42:51 -0400

    Fix a lot of bugs.

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -16,6 +16,7 @@
         msg.setAsMine();
       }
       $("ul.dialog").append(msg.render().$el);
+      $("main").scrollTop($("main").height());
     });
 
     this.socket.on("nicknameChanged", function(data){
@@ -30,6 +31,15 @@
 
     this.socket.on("addUser", function(data){
       $("ul.people").append("<li>" + data.nickname + "</li>");
+    });
+
+    this.socket.on("updateUsers", function(data){
+      $("ul.people").empty();
+
+      data.users.forEach(function(nickname){
+        $("ul.people").append("<li>" + nickname + "</li>");
+      });
+      console.log("I am here");
     });
 
     $("button.message-new").on("click", function(event){
